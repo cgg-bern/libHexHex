b2ecdf3edb815700dcca94d55424de2cda2456cd
Martin Heistermann <git@mheistermann.de>
2019-09-17 11:26:22 +0200

    predicates.c: Avoid out-of-bounds read.
    enow and fnow are only read if either smaller than the corresponding
    length, so not performing the read when we reached those lengths will
    not make any difference.
    
    This might seem harmless, however it is undefined behaviour and traps
    ASAN.

--- src/HexEx/predicates.c
+++ src/HexEx/predicates.c
@@ -941,10 +941,16 @@ REAL *h;
     while ((eindex < elen) && (findex < flen)) {
       if ((fnow > enow) == (fnow > -enow)) {
         Two_Sum(Q, enow, Qnew, hh);
-        enow = e[++eindex];
+        ++eindex;
+        if (eindex < elen) {
+            enow = e[eindex];
+        }
       } else {
         Two_Sum(Q, fnow, Qnew, hh);
-        fnow = f[++findex];
+        ++findex;
+        if (findex < flen) {
+            fnow = f[findex];
+        }
       }
       Q = Qnew;
       if (hh != 0.0) {
@@ -962,7 +968,10 @@ REAL *h;
   }
   while (findex < flen) {
     Two_Sum(Q, fnow, Qnew, hh);
-    fnow = f[++findex];
+    ++findex;
+    if (findex < flen) {
+        fnow = f[findex];
+    }
     Q = Qnew;
     if (hh != 0.0) {
       h[hindex++] = hh;
