b236fce69160fef14782fc24d733531f5bc46f31
David Bommes <dbommes@fantasio.inf.unibe.ch>
2022-06-22 11:10:33 +0200

    minor

--- src/HexEx/FileAccessor.cc
+++ src/HexEx/FileAccessor.cc
@@ -105,7 +105,7 @@ bool HexEx::writeToStream(std::ostream& os, TetrahedralMesh& mesh, PerCellVertex
       os << vh.idx() << std::endl;
 
     for(const auto& eh : ft_edges)
-      os << mesh.edge(eh).from_vertex().idx() << " " << mesh.edge(eh).to_vertex().idx() << std::endl;
+      os << mesh.halfedge(eh).from_vertex().idx() << " " << mesh.edge(eh).to_vertex().idx() << std::endl;
 
     for(const auto& fh : ft_faces)
     {
